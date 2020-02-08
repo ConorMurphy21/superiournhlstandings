169f569e4e0b0c7f96ae52b2a1944683320868f1
jengu <jen.jennifer.gu@gmail.com>
2020-02-08 14:20:42 -0800

    Starting conference standings and removing league stuff from regular standings

--- src/components/LeagueStandings.vue
+++ src/components/LeagueStandings.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <h5>
-            {{records}}
-        </h5>
+
     </div>
 </template>
 
--- src/components/Standings.vue
+++ src/components/Standings.vue
@@ -1,43 +1,39 @@
 <template>
+
   <div>
+    <h5>
+      {{records[5]}}
+    </h5>
     <b-btn-group>
       <b-button variant="secondary">Wild Card</b-button>
       <b-button variant="secondary">Division</b-button>
       <b-button variant="secondary">Conference</b-button>
       <b-button variant="secondary" autofocus>League</b-button>
     </b-btn-group>
-    <league-standings  v-bind:records="leagueRecords">
+    <league-standings  v-bind:records="records">
 
     </league-standings>
+<conference-standings v-bind:records="records">
 
+</conference-standings>
   </div>
+
 </template>
 
 <script>
     import LeagueStandings from "@/components/LeagueStandings";
+    import ConferenceStandings from "./ConferenceStandings";
     export default {
         name: "Standings",
         props:{
             system: String,
             records: []
         },
-        components: {LeagueStandings},
+        components: {ConferenceStandings, LeagueStandings},
         data(){
             return{
             }
-        },
-        computed: {
-          leagueRecords() {
-           let leagueRecords = [];
-            for(let i = 0;i < this.records.length; i++ ) {
-              for(let j = 0; j < this.records[i].teamRecords.length; j++) {
-                leagueRecords.push(this.records[i].teamRecords[j]);
-              }
-            }
-            return leagueRecords;
-          }
         }
-
     }
 </script>
 
