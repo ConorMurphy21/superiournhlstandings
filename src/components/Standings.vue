77f81d988f4973da96a8156decf000476d79ebb4
jengu <jen.jennifer.gu@gmail.com>
2020-02-08 10:57:26 -0800

    First successful pass of accessing NHL api

--- src/components/Standings.vue
+++ src/components/Standings.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-
+    <h1>
+      {{standings}}
+    </h1>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
 
-    export default {
-        name: "Standings"
+  export default {
+    data() {
+      return {
+        standings: [],
+        errors: []
+      }
+    },
+
+    // Fetches posts when the component is created.
+    created() {
+      axios.get(`https://statsapi.web.nhl.com/api/v1/standings`)
+              .then(response => {
+                // JSON responses are automatically parsed.
+                this.standings = response.data
+              })
+              .catch(e => {
+                this.errors.push(e)
+              })
     }
+  }
 </script>
 
 <style scoped>
