979f7a80e409593e963a755a25e8829bdfa4d3d3
Judd Foster <judd.foster2@gmail.com>
2020-02-08 13:29:14 -0800

    stuff yay

--- src/components/LeagueStandings.vue
+++ src/components/LeagueStandings.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
+    <table>
+        <standings-item>
 
-    </div>
+        </standings-item>
+    </table>
 </template>
 
 <script>
+    import StandingsItem from "./StandingsItem";
     export default {
         name: "LeagueStandings",
+        components: {StandingsItem},
+        data(){
+            return{
+                test: ["Boston","55","33","10","12","78","29","33","183","138","+45","18-2-9","15-8-3","0-7","7-2-1","W5"]
+            }
+        },
         props: {
             records: []
         },
