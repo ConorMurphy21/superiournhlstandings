b6456231117affebd0e207e3b74cbe0b5268562f
jengu <jen.jennifer.gu@gmail.com>
2020-02-17 22:39:40 -0800

    wildcard

--- src/components/standings/WildcardStandings.vue
+++ src/components/standings/WildcardStandings.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <h1 class="font-weight-bold">Eastern Conference</h1>
-        <generic-standings v-bind:records="easternRecords" name="Eastern Conference"></generic-standings>
         <generic-standings v-bind:records="metropolitanRecords" name="Metropolitan Division"></generic-standings>
         <generic-standings v-bind:records="atlanticRecords" name="Atlantic Division"></generic-standings>
+        <generic-standings v-bind:records="easternRecords" name="Eastern Conference"></generic-standings>
+
         <h1 class="font-weight-bold">Western Conference</h1>
-        <generic-standings v-bind:records="westernRecords" name="Western Conference"></generic-standings>
         <generic-standings v-bind:records="centralRecords" name="Central Division"></generic-standings>
         <generic-standings v-bind:records="pacificRecords" name="Pacific Division"></generic-standings>
+        <generic-standings v-bind:records="westernRecords" name="Western Conference"></generic-standings>
     </div>
 </template>
 
@@ -20,66 +21,65 @@
             records: Array
         },
         methods: {
-            wildRecords: function (cat) {
+            wildConfRecords: function (name, divTop1, divTop2) {
                 //Will use this method to sort
-                let wildRecords = [];
+                let wildConfRecords = [];
                 let teams = [];
                 for (let i = 0; i < this.records.length; i++) {
                     //if it finds the division, can break out as there is only one division
-                    if(this.records[i].division.name === cat) {
-                        for(let j = 0; j < this.records[i].teamRecords.length; j++) {
+                    if (this.records[i].conference.name === name) {
+                        for (let j = 0; j < this.records[i].teamRecords.length; j++) {
                             teams.push(this.records[i].teamRecords[j]);
                         }
-                        wildRecords.push(this.records[i]);
-                        teams.sort(function(a,b){return b.points - a.points});
-                        let team = teams.slice(0, 3);
-                        wildRecords.teamRecords = team;
+
+
+                        wildConfRecords.push(this.records[i]);
+                        wildConfRecords[i].teamRecords = teams;
                         break;
                     }
-
-                    //if it finds conference, needs to continue as there are two divisions in the conference
-                    if(this.records[i].conference.name === cat) {
-                        for(let j = 0; j < this.records[i].teamRecords.length; j++) {
+                }
+                return wildConfRecords;
+            },
+            divTopThree: function (name) {
+                let onlyTopThree = [];
+                let teams = [];
+                for (let i = 0; i < this.records.length; i++) {
+                    if (this.records[i].divison.name === name) {
+                        for (let j = 0; j < this.records[i].teamRecords.length; j++) {
                             teams.push(this.records[i].teamRecords[j]);
                         }
-                        wildRecords.push(this.records[i]);
-                        teams.sort(function(a,b){return b.points - a.points});
-                        let team = teams.splice(3, 4);
-                        wildRecords.teamRecords = team;
-                        continue;
-                    }
+                        teams.sort(function (a, b) {
+                            return b.points - a.points
+                        });
+                        teams = teams.slice(0, 3);
 
+                        onlyTopThree.push(this.records[i]);
+                        onlyTopThree[0].teamRecords = teams;
+                        break;
+
+                    }
                 }
-                return wildRecords;
+                return onlyTopThree;
             }
         },
         computed:{
             atlanticRecords() {
-                let cat = "Atlantic";
-                return this.wildRecords(cat);
+                return this.divTopThree("Atlantic");
             },
             metropolitanRecords() {
-                let cat = "Metropolitan";
-                return this.wildRecords(cat);
-
+                return this.divTopThree("Metropolitan");
             },
             easternRecords() {
-                let cat = "Eastern";
-                return this.wildRecords(cat);
-
+                return this.wildRecords("Eastern", this.metropolitanRecords.teamRecords, this.atlanticRecords.teamRecords);
+            },
+            centralRecords(){
+                return this.divTopThree("Central");
             },
             pacificRecords(){
-                let cat = "Pacific";
-                return this.wildRecords(cat);
-            },
-
-            centralRecords(){
-                let cat = "Central";
-                return this.wildRecords(cat);
+                return this.divTopThree("Pacific");
             },
             westernRecords() {
-                let cat = "Western";
-                return this.wildRecords(cat);
+                return this.wildRecords("Eastern", this.atlanticRecords.teamRecords, this.metropolitanRecords.teamRecords);
             }
         }
     }
