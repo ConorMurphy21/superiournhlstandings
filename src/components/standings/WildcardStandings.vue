a05668d7fb020abecc9f909c9f5009b7dc726002
jengu <jen.jennifer.gu@gmail.com>
2020-02-24 00:06:45 -0800

    uncutdown wildcard

--- src/components/standings/WildcardStandings.vue
+++ src/components/standings/WildcardStandings.vue
@@ -1,14 +1,39 @@
 <template>
     <div>
         <h1 class="font-weight-bold">Eastern Conference</h1>
-        <generic-standings v-bind:records="metropolitanRecords" name="Metropolitan Division"></generic-standings>
-        <generic-standings v-bind:records="atlanticRecords" name="Atlantic Division"></generic-standings>
-        <generic-standings v-bind:records="easternRecords" name="Eastern Conference"></generic-standings>
+        <generic-standings :records="metropolitanRecords"
+                           :sort-by.sync="sortBy"
+                           :sort-desc.sync="sortDesc"
+                           name="Metropolitan Division">
+        </generic-standings>
+        <generic-standings :records="atlanticRecords"
+                           :sort-by.sync="sortBy"
+                           :sort-desc.sync="sortDesc"
+                           name="Atlantic Division">
+        </generic-standings>
+        <generic-standings :records="easternRecords"
+                           :sort-by.sync="sortBy"
+                           :sort-desc.sync="sortDesc"
+                           name="Eastern Conference">
+
+        </generic-standings>
 
         <h1 class="font-weight-bold">Western Conference</h1>
-        <generic-standings v-bind:records="centralRecords" name="Central Division"></generic-standings>
-        <generic-standings v-bind:records="pacificRecords" name="Pacific Division"></generic-standings>
-        <generic-standings v-bind:records="westernRecords" name="Western Conference"></generic-standings>
+        <generic-standings :records="centralRecords"
+                           :sort-by.sync="sortBy"
+                           :sort-desc.sync="sortDesc"
+                           name="Central Division">
+        </generic-standings>
+        <generic-standings :records="pacificRecords"
+                           :sort-by.sync="sortBy"
+                           :sort-desc.sync="sortDesc"
+                           name="Pacific Division">
+        </generic-standings>
+        <generic-standings :records="westernRecords"
+                           :sort-by.sync="sortBy"
+                           :sort-desc.sync="sortDesc"
+                           name="Western Conference">
+        </generic-standings>
     </div>
 </template>
 
@@ -20,19 +45,26 @@
         props: {
             records: Array
         },
+        data(){
+            return{
+                sortBy: "points",
+                sortDesc: true
+            }
+        },
         methods: {
             wildConfRecords: function (name, divTop1, divTop2) {
                 //Will use this method to sort
                 let wildConfRecords = [];
                 let teams = [];
+                let divTop = divTop1.concat(divTop2);
                 for (let i = 0; i < this.records.length; i++) {
                     //if it finds the division, can break out as there is only one division
                     if (this.records[i].conference.name === name) {
                         for (let j = 0; j < this.records[i].teamRecords.length; j++) {
-                            teams.push(this.records[i].teamRecords[j]);
+                            if(!divTop.includes(this.records[i].teamRecords[j])){
+                                teams.push(this.records[i].teamRecords[j]);
+                            }
                         }
-
-
                         wildConfRecords.push(this.records[i]);
                         wildConfRecords[i].teamRecords = teams;
                         break;
@@ -45,16 +77,19 @@
                 let teams = [];
                 for (let i = 0; i < this.records.length; i++) {
                     if (this.records[i].divison.name === name) {
+
                         for (let j = 0; j < this.records[i].teamRecords.length; j++) {
                             teams.push(this.records[i].teamRecords[j]);
                         }
-                        teams.sort(function (a, b) {
-                            return b.points - a.points
-                        });
+
+                       teams.sort(function (a, b) {
+                           return b.points - a.points
+                       });
+
                         teams = teams.slice(0, 3);
 
+                        this.records[i].teamRecords = teams;
                         onlyTopThree.push(this.records[i]);
-                        onlyTopThree[0].teamRecords = teams;
                         break;
 
                     }
@@ -67,19 +102,27 @@
                 return this.divTopThree("Atlantic");
             },
             metropolitanRecords() {
-                return this.divTopThree("Metropolitan");
+                return this.records;
+                //return this.divTopThree("Metropolitan");
             },
             easternRecords() {
-                return this.wildRecords("Eastern", this.metropolitanRecords.teamRecords, this.atlanticRecords.teamRecords);
+                return this.records;
+               //return this.wildConfRecords("Eastern", this.metropolitanRecords.teamRecords, this.atlanticRecords.teamRecords);
             },
             centralRecords(){
-                return this.divTopThree("Central");
+                return this.records;
+
+//                return this.divTopThree("Central");
             },
             pacificRecords(){
-                return this.divTopThree("Pacific");
+                return this.records;
+
+//                return this.divTopThree("Pacific");
             },
             westernRecords() {
-                return this.wildRecords("Eastern", this.atlanticRecords.teamRecords, this.metropolitanRecords.teamRecords);
+                return this.records;
+
+                // return this.wildConfRecords("Eastern", this.atlanticRecords.teamRecords, this.metropolitanRecords.teamRecords);
             }
         }
     }
